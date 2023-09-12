a7be014687cfcbb05045e47d89cae38f067719e4
thayM <thay.contato1@gmail.com>
2023-09-12 20:39:59 -0300

    feature css da página de login

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style_login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Página de Login</title>
+</head>
+<body>
+
+<form action="">
+    <h2>Login</h2>
+
+    <div class="campos">
+    <label for="">Email</label>
+    <input type="text">
+
+    <label for="">Senha</label>
+    <input type="text">
+    </div>
+    
+    <p>Esqueci minha senha</p>
+    <button>Logar</button>
+
+    <p>ou</p>
+
+    <button>Google</button>
+    <button>Facebook</button>
+
+    <p>Não tem conta? Clique para se cadastrar</p>
+</form>
+    
+</body>
+</html>
